1a00a039fd4a1c95f763cdb60e544b8d6fd3d51b
Wei Wu <wuw@snaptracs.com>
2015-07-29 14:25:36 -0700

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'http://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
+gem 'bootstrap-sass',       '3.2.0.0'
+
 
 group :development, :test do
   gem 'sqlite3',     '1.3.9'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | Dala Horse"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Dala Horse"
+  end
+
 end
