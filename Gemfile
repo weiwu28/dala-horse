2c91dbee90c1a215d2238f1b3f00811c25beb429
Wei Wu <wuw@snaptracs.com>
2015-08-03 13:41:44 -0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ source 'http://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 gem 'bcrypt',               '3.1.7' # needed by has_secure_password
+gem 'faker',                '1.4.2'
+gem 'will_paginate',           '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 gem 'bootstrap-sass',       '3.2.0.0'
 
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -39,8 +39,23 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
