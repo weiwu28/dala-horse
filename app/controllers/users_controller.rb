1738a73a98cfd754bf22a2b9d4d292eacea9a692
Wei Wu <wuw@snaptracs.com>
2015-08-06 16:40:21 -0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -46,19 +47,13 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
   def correct_user
     @user = User.find(params[:id])
